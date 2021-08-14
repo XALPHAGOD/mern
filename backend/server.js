b34e6dd9f01d35abacca54cff2c244618f2cb31c
XALPHAGOD <gamingsoumyaakhuli@gmail.com>
2021-08-14 14:35:15 +0530

    Bef Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const dotenv = require("dotenv");
 const configOptions = { path: "../.env" }; //depends on from which directory started running
@@ -15,10 +16,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.BACKEND_SERVER_PORT || 5000;
-// conn(); //db connection
+conn(); //db connection
 
 app.use("/users", usersRoutes);
 app.use("/user", verifyUser, userRoutes);
+
+// for deployment
+const dirname = path.resolve();
+if (process.env.ENV === "prod") {
+  //   console.log(path.resolve());
+  app.use(express.static(path.join(dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirname, "frontend", "build"));
+  });
+} else {
+}
 
 app.use(errHandler); //error handlers always at last to listen to err objs from any routes
 
